c331fab3c88508438ed1a571a85279896b354315
Sascha Koch <sascha.koch@web.de>
2019-03-31 13:08:58 +0200

    initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # insult
-Fortune-like program that insults the caller
+'fortune'-like program that insults the caller. Insults have several sources; initial idea came from here https://www.reddit.com/r/AskReddit/comments/b758ow/what_is_the_harshest_grated_insult_you_have/
+
+Please be aware!
+
+> Some insults may hurt your feelings although they are g-rated.
+
+## Usage
+
+  $ gcc insult.c -o insult
+
+
---
+++ insult.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char *insults[27] = {
+  "You are more disappointing than an unsalted pretzel.",
+  "You look like you came from a donation pile.",
+  "You need to go home and think about everything you are. Then change it.",
+  "I think we've all got something to bring to this discussion, and from now on I think the thing you should bring is silence.",
+  "Over the many years I have known all of you, I have come to regard you as....people I met.",
+  "That was an important speech and it needed to be made. Might I suggest, however, that the rest of this discourse is continued by those with brains larger than a grape?",
+  "Stupid people can believe in anything, so you can believe in yourself!",
+  "It's impossible to underestimate you!",
+  "Wow, I expected nothing from you and am still disappointed.",
+  "I wish you were a speed bump for my tricycle.",
+  "You are a gray sprinkle on a rainbow cupcake.",
+  "You're like a lighthouse in the middle of a desert. Bright, but not a lot of use.",
+  "This is why everyone talks about you behind your back.",
+  "The worst thing about your guitar playing is that you don't know how bad you are at it.",
+  "Why are you such a wet sandwich?",
+  "If Mr. Rogers was your neighbor, he'd move.",
+  "You're less a person and more a loose collection of personality flaws.",
+  "You're like God spilled a person.",
+  "Your life is more about regret management than goal achievement, isn't it?",
+  "Everyone that has ever loved you was wrong.",
+  "The only culture you have is bacteria.",
+  "You're not pretty enough to get away with being that stupid.",
+  "Your grades say 'marry rich,' but your face says 'study harder'.",
+  "Bob Ross would call you a mistake.",
+  "Apologize to that tree over there for wasting the oxygen that it worked so hard to make.",
+  "If scientists found a way to convert brainpower into gasoline, you wouldn't have enough fuel to power an ant's go kart around the inside of a fruit loop.",
+  "You’re the patient zero of dumbness aren’t you?",
+};
+
+
+int main() {
+  time_t t;
+
+  srand((unsigned) time(&t));
+  int r = rand() % (sizeof(insults) / sizeof(insults[0]));
+
+  printf("%s\n", insults[r]);
+  return 0;
+}
+
